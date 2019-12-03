c414dd4e5149ef6e775d552343ff760c591389e3
Vyacheslav Khorkov <artfintch@ya.ru>
2019-12-04 00:53:18 +0500

    Add day03 fast solution.

--- Swift/Sandbox/main.swift
+++ Swift/Sandbox/main.swift
@@ -9,31 +9,85 @@
 import Frog
 
 let frog = Frog("input.txt")
-var numbers = frog
-    .readLine()!
-    .components(separatedBy: ",")
-    .compactMap(Int.init)
-
-func tryInput(_ numbers: [Int], _ a: Int, _ b: Int) -> Int {
-    var numbers = numbers
-    numbers[1] = a
-    numbers[2] = b
-    for start in stride(from: 0, to: numbers.count, by: 4) {
-        if numbers[start] == 99 { break }
-        let op: (Int, Int) -> Int = (numbers[start] == 1) ? (+) : (*)
-        let (arg0, arg1) = (numbers[start + 1], numbers[start + 2])
-        let output = numbers[start + 3]
-        numbers[output] = op(numbers[arg0], numbers[arg1])
-    }
-    return numbers[0]
+let lines = frog.readLines().map {
+    $0.components(separatedBy: ",")
 }
 
-print(tryInput(numbers, 12, 2))
-for a in 0..<100 {
-    for b in 0..<100 {
-        if tryInput(numbers, a, b) == 19690720 {
-            print(a * 100 + b)
-            break
+struct Point: Hashable {
+    var x: Int, y: Int
+}
+
+var id = 0
+var cross: Set<Point> = []
+var points: [Set<Point>] = [[], []]
+var steps: [[Point: Int]] = [[:], [:]]
+for line in lines {
+    var stepsCount = 0
+    let anotherId = Swift.abs(id - 1)
+    var current = Point(x: 0, y: 0)
+    for step in line {
+        let direction = step.first!
+        let distance = Int(step.dropFirst())!
+        switch direction {
+        case "U":
+            ((current.y + 1)...(current.y + distance)).forEach {
+                stepsCount += 1
+                let point = Point(x: current.x, y: $0)
+                points[id].insert(point)
+                steps[id][point] = stepsCount
+                if points[anotherId].contains(point) {
+                    cross.insert(point)
+                }
+            }
+            current.y += distance
+
+        case "D":
+            ((current.y - distance)..<current.y).reversed().forEach {
+                stepsCount += 1
+                let point = Point(x: current.x, y: $0)
+                points[id].insert(point)
+                steps[id][point] = stepsCount
+                if points[anotherId].contains(point) {
+                    cross.insert(point)
+                }
+            }
+            current.y -= distance
+
+        case "L":
+            ((current.x - distance)..<current.x).reversed().forEach {
+                stepsCount += 1
+                let point = Point(x: $0, y: current.y)
+                points[id].insert(point)
+                steps[id][point] = stepsCount
+                if points[anotherId].contains(point) {
+                    cross.insert(point)
+                }
+            }
+            current.x -= distance
+
+        case "R":
+            ((current.x + 1)...(current.x + distance)).forEach {
+                stepsCount += 1
+                let point = Point(x: $0, y: current.y)
+                points[id].insert(point)
+                steps[id][point] = stepsCount
+                if points[anotherId].contains(point) {
+                    cross.insert(point)
+                }
+            }
+            current.x += distance
+
+        default:
+            fatalError()
         }
     }
+    id += 1
 }
+
+let min = cross.map { Swift.abs($0.x) + Swift.abs($0.y) }.min()!
+print(min)
+
+let min2 = cross.map {
+    return steps[0][$0]! + steps[1][$0]!
+}.min()!
+print(min2)
