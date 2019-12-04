203b672239531143015ba94d1185d984fd8675d3
Vyacheslav Khorkov <artfintch@ya.ru>
2019-12-04 17:55:22 +0500

    Add day04 fast solution.

--- Swift/Sandbox/main.swift
+++ Swift/Sandbox/main.swift
@@ -8,86 +8,18 @@
 
 import Frog
 
-let frog = Frog("input.txt")
-let lines = frog.readLines().map {
-    $0.components(separatedBy: ",")
-}
+func isValidPass(_ pass: Int, uniqueCount: (Int) -> Bool) -> Bool {
+    let string = String(pass)
+    guard String(string.sorted()) == string else { return false }
 
-struct Point: Hashable {
-    var x: Int, y: Int
-}
-
-var id = 0
-var cross: Set<Point> = []
-var points: [Set<Point>] = [[], []]
-var steps: [[Point: Int]] = [[:], [:]]
-for line in lines {
-    var stepsCount = 0
-    let anotherId = Swift.abs(id - 1)
-    var current = Point(x: 0, y: 0)
-    for step in line {
-        let direction = step.first!
-        let distance = Int(step.dropFirst())!
-        switch direction {
-        case "U":
-            ((current.y + 1)...(current.y + distance)).forEach {
-                stepsCount += 1
-                let point = Point(x: current.x, y: $0)
-                points[id].insert(point)
-                steps[id][point] = stepsCount
-                if points[anotherId].contains(point) {
-                    cross.insert(point)
-                }
-            }
-            current.y += distance
-
-        case "D":
-            ((current.y - distance)..<current.y).reversed().forEach {
-                stepsCount += 1
-                let point = Point(x: current.x, y: $0)
-                points[id].insert(point)
-                steps[id][point] = stepsCount
-                if points[anotherId].contains(point) {
-                    cross.insert(point)
-                }
-            }
-            current.y -= distance
-
-        case "L":
-            ((current.x - distance)..<current.x).reversed().forEach {
-                stepsCount += 1
-                let point = Point(x: $0, y: current.y)
-                points[id].insert(point)
-                steps[id][point] = stepsCount
-                if points[anotherId].contains(point) {
-                    cross.insert(point)
-                }
-            }
-            current.x -= distance
-
-        case "R":
-            ((current.x + 1)...(current.x + distance)).forEach {
-                stepsCount += 1
-                let point = Point(x: $0, y: current.y)
-                points[id].insert(point)
-                steps[id][point] = stepsCount
-                if points[anotherId].contains(point) {
-                    cross.insert(point)
-                }
-            }
-            current.x += distance
-
-        default:
-            fatalError()
-        }
+    let counts: [Character: Int] = string.reduce(into: [:]) {
+        $0[$1, default: 0] += 1
     }
-    id += 1
+    return counts.values.contains(where: uniqueCount)
 }
 
-let min = cross.map { Swift.abs($0.x) + Swift.abs($0.y) }.min()!
-print(min)
-
-let min2 = cross.map {
-    return steps[0][$0]! + steps[1][$0]!
-}.min()!
-print(min2)
+let range = 197487..<673251
+let silver = range.reduce(0) { $0 + (isValidPass($1, uniqueCount: { $0 > 1 }) ? 1 : 0) }
+assert(silver == 1640)
+let gold = range.reduce(0) { $0 + (isValidPass($1, uniqueCount: { $0 == 2 }) ? 1 : 0) }
+assert(gold == 1126)
