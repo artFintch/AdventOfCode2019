f156e1aa57a48e26da680004471b75e68d6d455f
Vyacheslav Khorkov <artfintch@ya.ru>
2019-12-09 23:15:42 +0500

    Add day09 fast solution.
    # Conflicts:
    #	Swift/Sandbox/input.txt
    #	Swift/Sandbox/main.swift

--- Swift/Sandbox/main.swift
+++ Swift/Sandbox/main.swift
@@ -8,70 +8,126 @@
 
 import Frog
 
-let line = Frog("input.txt").readLine()!.compactMap {
-    Int(String($0))
+let line = Frog("input.txt").readLine()!
+    .components(separatedBy: ",")
+    .compactMap(Int.init)
+
+func getDigits(num: Int) -> [Int] {
+    var digits: [Int] = []
+    var num = num
+    while num > 0 {
+        let lastDigit = num % 10
+        num /= 10
+        digits.append(lastDigit)
+    }
+    return digits
 }
 
-var current = 0
-var columnSize = 6
-var rowSize = 25
-var layers = line.count/(columnSize*rowSize)
-var map: [[[Int]]] = Array(repeating: Array(repeating: Array(repeating: -1, count: rowSize), count: columnSize), count: layers)
+func runProgram(_ nums: [Int], input: Int) -> Int {
+    var input = input
+    var dict: [Int: Int] = [:]
+    for pair in nums.enumerated() {
+        dict[pair.offset] = pair.element
+    }
 
-for layer in 0..<layers {
-    for column in 0..<columnSize {
-        for row in 0..<rowSize {
-            map[layer][column][row] = line[current]
-            current += 1
+    var index = 0
+    var rel = 0
+
+    func indexForMode(_ mode: Int, i: Int, r: Int) -> Int {
+        switch mode {
+        case 0:
+            return dict[i, default: 0]
+        case 1:
+            return i
+        case 2:
+            return r + dict[i, default: 0]
+        default:
+            fatalError()
+        }
+    }
+
+    while true {
+        var digits = getDigits(num: dict[index]!)
+
+        let op: Int
+        if digits.count == 1 {
+            op = digits.removeFirst()
+        } else {
+            op = digits[1] * 10 + digits.removeFirst()
+            digits.removeFirst()
+        }
+
+        let mode0 = digits.isEmpty ? 0 : digits.removeFirst()
+        let mode1 = digits.isEmpty ? 0 : digits.removeFirst()
+        let mode2 = digits.isEmpty ? 0 : digits.removeFirst()
+
+        switch op {
+        case 1:
+            // +
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            dict[indexForMode(mode2, i: index + 3, r: rel)] = arg1 + arg2
+            index += 4
+
+        case 2:
+            // *
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            dict[indexForMode(mode2, i: index + 3, r: rel)] = arg1 * arg2
+            index += 4
+
+        case 3:
+            // in
+            dict[indexForMode(mode0, i: index + 1, r: rel)] = input
+            index += 2
+
+        case 4:
+            // out
+            input = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            index += 2
+
+        case 5:
+            // jump-if-true
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            index = (arg1 != 0) ? arg2 : index + 3
+
+        case 6:
+            // jump-if-false
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            index = (arg1 == 0) ? arg2 : index + 3
+
+        case 7:
+            // less
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            dict[indexForMode(mode2, i: index + 3, r: rel)] = (arg1 < arg2 ? 1 : 0)
+            index += 4
+
+        case 8:
+            // equals
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            dict[indexForMode(mode2, i: index + 3, r: rel)] = (arg1 == arg2 ? 1 : 0)
+            index += 4
+
+        case 9:
+            // relative
+            rel += dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            index += 2
+
+        case 99:
+            return input
+
+        default:
+            fatalError()
         }
     }
 }
 
-var counts: [Int] = []
-for layer in 0..<layers {
-    counts.append(
-        map[layer].flatMap { $0 }.reduce(0) { $0 + (($1 == 0) ? 1 : 0) }
-    )
-}
-
-var imin = -1
-var min = 999999
-for i in counts.indices {
-    if counts[i] < min {
-        min = counts[i]
-        imin = i
-    }
-}
-
-print(map[imin])
-let a = map[imin].flatMap { $0 }.reduce(0) { $0 + (($1 == 1) ? 1 : 0) }
-let b = map[imin].flatMap { $0 }.reduce(0) { $0 + (($1 == 2) ? 1 : 0) }
-print(a * b)
-// 1224
+// silver
+assert(runProgram(line, input: 1) == 3241900951)
 
 // gold
-
-var image: [[Int]] = Array(repeating: Array(repeating: 0, count: rowSize), count: columnSize)
-for column in 0..<columnSize {
-    for row in 0..<rowSize {
-        for layer in 0..<layers {
-            if map[layer][column][row] == 0 {
-                // black
-                image[column][row] = map[layer][column][row]
-                break
-            } else if map[layer][column][row] == 1 {
-                // white
-                image[column][row] = map[layer][column][row]
-                break
-            } else {
-                // transparent
-                continue
-            }
-        }
-    }
-}
-
-for row in image {
-    print(row.map { $0 == 1 ? "#" : "." })
-}
-// EBZUR
+assert(runProgram(line, input: 2) == 83089)
