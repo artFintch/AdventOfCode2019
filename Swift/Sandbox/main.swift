4b37c2b6955009e8745ba7622ccb8c848424c489
Vyacheslav Khorkov <artfintch@ya.ru>
2019-12-18 01:01:10 +0500

    Add day 17 silver fast solution.
    # Conflicts:
    #	Swift/Sandbox/input.txt
    #	Swift/Sandbox/main.swift

--- Swift/Sandbox/main.swift
+++ Swift/Sandbox/main.swift
@@ -8,115 +8,168 @@
 
 import Frog
 
-let input = Frog("input.txt").readLine()!
-    .map(String.init)
-    .compactMap(Int.init)
-
-//var cache: [String: [Int]] = [:]
-func getMult(row: Int, column: Int) -> Int {
-    let width = 4 * (row + 1)
-    let index = (column + 1) % width
-    let localIndex = index / (row + 1)
-    return [0, 1, 0, -1][localIndex]
-
-
-//    let k = "\(column),\(row)"
-//    let a = column/((row+1)*4)+2
-//    if cache[k] != nil {
-//        return cache[k]![column + 1]
-//    }
-//
-//    let base = [0, 1, 0, -1]
-//    var new: [Int] = []
-//    for index in base {
-//        for _ in 0..<(row+1) {
-//            new.append(index)
-//        }
-//    }
-
-//    [0, 0, 1, 1, 0, 0, -1, -1]
-//    return base[(((column + row) % base.count) + 1) % base.count]
-
-
-//    var new2: [Int] = []
-//    for _ in 0...a {
-//        new2.append(contentsOf: new)
-//    }
-//    cache[k] = new2
-//    return new2[column + 1]
+func readInput(from path: String) -> [Int] {
+    return Frog(path).readLine()!
+        .components(separatedBy: ",")
+        .compactMap(Int.init)
 }
 
-print(getMult(row: 0, column: 0) == 1)
-print(getMult(row: 0, column: 1) == 0)
-print(getMult(row: 0, column: 2) == -1)
-print(getMult(row: 0, column: 3) == 0)
-print(getMult(row: 0, column: 4) == 1)
-
-print(getMult(row: 1, column: 5) == -1)
-print(getMult(row: 1, column: 1) == 1)
-print(getMult(row: 6, column: 5) == 0)
-
-
-var ar: [[Int]] = Array(repeating: [], count: input.count)
-func runPhase(input: [Int]) -> [Int] {
-    var output: [Int] = []
-    for row in input.indices {
-//        var line = ""
-        var sum = 0
-        for column in input.indices {
-            sum += input[column] * getMult(row: row, column: column)
-//            print(column, a)
-//            line += String("\(input[column])*\(getMult(row: row, column: column)) ")
-        }
-//        print(Double(row)/Double(input.count))
-        let a = abs(sum % 10)
-        output.append(a)
-        ar[row].append(sum)
+func getDigits(num: Int) -> [Int] {
+    var digits: [Int] = []
+    var num = num
+    while num > 0 {
+        let lastDigit = num % 10
+        num /= 10
+        digits.append(lastDigit)
     }
-    return output
+    return digits
 }
 
-func printNums(_ nums: [Int], _ nums2: [Int]) {
-    print(nums.map(String.init).joined(), nums2.map(String.init).joined())
+func runProgram(_ nums: [Int], input: () -> Int, output: (Int) -> Void) {
+    var dict: [Int: Int] = [:]
+    for pair in nums.enumerated() {
+        dict[pair.offset] = pair.element
+    }
+
+    var index = 0
+    var rel = 0
+
+    func indexForMode(_ mode: Int, i: Int, r: Int) -> Int {
+        switch mode {
+        case 0:
+            return dict[i, default: 0]
+        case 1:
+            return i
+        case 2:
+            return r + dict[i, default: 0]
+        default:
+            fatalError()
+        }
+    }
+
+    while true {
+        var digits = getDigits(num: dict[index]!)
+
+        let op: Int
+        if digits.count == 1 {
+            op = digits.removeFirst()
+        } else {
+            op = digits[1] * 10 + digits.removeFirst()
+            digits.removeFirst()
+        }
+
+        let mode0 = digits.isEmpty ? 0 : digits.removeFirst()
+        let mode1 = digits.isEmpty ? 0 : digits.removeFirst()
+        let mode2 = digits.isEmpty ? 0 : digits.removeFirst()
+
+        switch op {
+        case 1:
+            // +
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            dict[indexForMode(mode2, i: index + 3, r: rel)] = arg1 + arg2
+            index += 4
+
+        case 2:
+            // *
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            dict[indexForMode(mode2, i: index + 3, r: rel)] = arg1 * arg2
+            index += 4
+
+        case 3:
+            // in
+            dict[indexForMode(mode0, i: index + 1, r: rel)] = input()
+            index += 2
+
+        case 4:
+            // out
+            output(dict[indexForMode(mode0, i: index + 1, r: rel), default: 0])
+            index += 2
+
+        case 5:
+            // jump-if-true
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            index = (arg1 != 0) ? arg2 : index + 3
+
+        case 6:
+            // jump-if-false
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            index = (arg1 == 0) ? arg2 : index + 3
+
+        case 7:
+            // less
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            dict[indexForMode(mode2, i: index + 3, r: rel)] = (arg1 < arg2 ? 1 : 0)
+            index += 4
+
+        case 8:
+            // equals
+            let arg1 = dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            let arg2 = dict[indexForMode(mode1, i: index + 2, r: rel), default: 0]
+            dict[indexForMode(mode2, i: index + 3, r: rel)] = (arg1 == arg2 ? 1 : 0)
+            index += 4
+
+        case 9:
+            // relative
+            rel += dict[indexForMode(mode0, i: index + 1, r: rel), default: 0]
+            index += 2
+
+        case 99:
+            return
+
+        default:
+            fatalError()
+        }
+    }
 }
 
-//print(5978017%650)
-//617
-var nums = input
-//let offset = Int(nums[0..<7].map(String.init).joined())
+//
+var input = readInput(from: "input.txt")
 
-//var nums2 = input + input
-//printNums(nums, nums2)
-var phase = 0
-while phase < 100 {
-    phase += 1
-    nums = runPhase(input: nums)
-//    nums2 = runPhase(input: nums2)
-//    printNums(nums, [])
-//    print("phase:", phase)
+var map: [[Int]] = []
+var row: [Int] = []
+runProgram(input, input: { () -> Int in
+    fatalError()
+}, output: {
+    switch $0 {
+    case 46:
+        row.append(0)
+    case 35:
+        row.append(1)
+    case 10:
+        if !row.isEmpty {
+            map.append(row)
+            row.removeAll()
+        }
+    default:
+//        fatalError()
+        print($0)
+        break
+    }
+})
+map.forEach {
+    print($0.map { $0 == 1 ? "#" : "." })
 }
-//print(nums[617..<625].map(String.init).joined())
 
-for (i, aa) in ar.enumerated() {
-    print(i, aa)
+var count = 0
+var sum = 0
+for i in 1..<map.count-1 {
+    for j in 1..<map[i].count-1 {
+        if map[i][j] == 1 && map[i-1][j] == 1 && map[i+1][j] == 1 && map[i][j-1] == 1 && map[i][j+1] == 1 {
+            sum += i * j
+            count += 1
+            map[i][j] = 2
+        }
+    }
 }
+print(count, sum)
 
-//var nums: [Int] = []
-//for _ in 0..<1_000 {
-//    nums.append(contentsOf: input)
-//}
-
-//var phase = 0
-//while phase < 100 {
-//    phase += 1
-//    nums = runPhase(input: nums)
-//    print("phase:", phase)
-//}
-//print(nums[0..<8].map(String.init).joined())
-
-
-// 27, 17, 15, 33, 17
-// 20, 10, 8, 28, 14
-// 16, 10, 8, 20, 6
-// 10, 4, 8, 12, 6
-// 4, 4, 4, 4, 4
+map.forEach {
+    print($0.map {
+        $0 == 1 ? "#" : $0 == 2 ? "O" : "."
+    })
+}
